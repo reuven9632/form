0427b54e9433fcab82d6e812b3bfc0933b0a88a2
reuven9632 <reuven9632@gmail.com>
2020-11-09 23:54:57 +0200

    boot

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include_once "php/head.php";?>
+
+    <div class="container">
+        <div class="forms">
+            <form action="php/formAuthorization.php" classs="formAuthorization" method="post">
+                <h2>FORM Authorization</h2>
+                <label>
+                Enter your name
+                    <input type="text" name="name">
+                </label>
+                <label>
+                Enter your fename
+                    <input type="text" name="fename">
+                </label>
+            </form>
+
+            <form action="php/formRegistration.php" classs="formRegistration" method="post">
+                <h2>FORM Registration</h2>
+                <label>
+                Enter your fename
+                    <input type="text" name="fename">
+                </label>
+            </form>
+        </div>
+    </div>
+
+<?php include_once "php/footer.php";?>
