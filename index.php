b947983c63eee05718291c3eb74bb7c890a98d25
reuven9632 <66059644+reuven9632@users.noreply.github.com>
2020-11-10 08:47:39 +0200

    make dom

--- index.php
+++ index.php
@@ -9,17 +9,23 @@
                     <input type="text" name="name">
                 </label>
                 <label>
-                Enter your fename
-                    <input type="text" name="fename">
+                Enter your female
+                    <input type="text" name="female">
                 </label>
+                <button type="submit" class="done-btn">done</button>
             </form>
 
             <form action="php/formRegistration.php" classs="formRegistration" method="post">
                 <h2>FORM Registration</h2>
                 <label>
-                Enter your fename
-                    <input type="text" name="fename">
+                Enter your name
+                    <input type="text" name="name">
                 </label>
+                <label>
+                Enter your female
+                    <input type="text" name="female">
+                </label>
+                <button type="submit" class="send-btn">send</button>
             </form>
         </div>
     </div>
